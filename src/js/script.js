10d181b5c36151674170ffe19cc04eeee2b4ce18
Alexandre <rm558270@fiap.com.br>
2024-05-02 11:01:48 -0300

    criando a função adicionar tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,31 @@
-//Declarando as variáveis
+//Declarando as variaveis
+let tarefas=[];
 
-let tarefa=[];
+//Função que valida o preenchimento do campo caso esteja vazio
 
-//função que valida o preenchimento do campo caso esteja vazio
-
-function validaCampo(){
-
-    let valida = false;
-    
-    if(document.getElementById("task").value == "")
-    valida=true;
+function validaCampo()
+{
+    let valida =false;
+    if(document.getElementById("task").value =="") 
+    valida =true;
     return valida;
+}
+//Função que adiciona a tarefa
 
+function adicionarTarefa(){
+        //criando a variavel que vai receber as tarefas
+    let linhas =document.getElementById("task");
+
+    if(validaCampo()){
+        alert("Preencha o campo com a tarefa")
+    }else{
+        //adicionando as tarefas
+        tarefas.push(linhas.value);
+        //passando para a proxima linha vazia
+        linhas.value="";
+        //chamando a função que vai mostrar todas as tarefas
+        listarTarefas();
+    }
+    //volta para o input da tarefa
+    document.getElementById("task").focus();
 }
